a9c81ae8e8184d265f0c5a14938c1ff07847e14b
sdboer78 <sander@deboernet.eu>
2025-04-04 15:28:53 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,19 +18,7 @@ export default function RootLayout({ children }) {
           crossOrigin="anonymous"
         />
         <link rel="stylesheet" href="https://cdn.auth0.com/js/auth0-samples-theme/1.0/css/auth0-theme.min.css" />
-     <script>
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://deboernet.matomo.cloud/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='https://cdn.matomo.cloud/deboernet.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
+
       </head>
       <body>
         <UserProvider>
